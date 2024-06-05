d52e57aa454d9de406f219e98e3712cbd6c2b4fd
Paul5400 <160632943+Paul5400@users.noreply.github.com>
2024-06-05 10:03:53 +0200

    hehe

---
+++ projet_etu/zeldiablo/src/gameLaby/laby/CaseDeclencheur.java
@@ -0,0 +1,24 @@
+package gameLaby.laby;
+
+public abstract class CaseDeclencheur {
+
+
+    private int x,y;
+
+
+
+
+
+    public CaseDeclencheur(int dx,int dy){
+        x=dx;
+        y=dy;
+    }
+    abstract void event(Personnage p);
+    public int getY() {
+        return y;
+    }
+    public int getX() {
+        return x;
+    }
+
+}
---
+++ projet_etu/zeldiablo/src/gameLaby/laby/CasePiege.java
@@ -0,0 +1,32 @@
+package gameLaby.laby;
+
+public class CasePiege extends CaseDeclencheur {
+    boolean ammorce;
+    boolean passerDessus;
+
+    public CasePiege(int dx, int dy) {
+        super(dx, dy);
+        ammorce = true;
+        passerDessus = false;
+    }
+
+    @Override
+    public void event(Personnage p) {
+        if (p.etrePresent(this.getX(), this.getY())) {
+            if (ammorce) {
+                System.out.println("boom");
+                ammorce = false;
+                p.ajouterVie(-10);
+                passerDessus = true;
+            }
+        } else {
+            ammorce = true;
+        }
+    }
+
+    public boolean getPasserDessus() {
+        return passerDessus;
+    }
+
+
+}
