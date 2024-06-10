e589a940247b2500d0ec47a6942525fe3c619551
baki33a <robin.carette6@etu.univ-lorraine.fr>
2024-06-10 08:48:15 +0200

    classes monstre et Intelligence

--- projet_etu/zeldiablo/src/gameLaby/laby/Monstre.java
+++ projet_etu/zeldiablo/src/gameLaby/laby/Monstre.java
@@ -1,6 +1,5 @@
 package gameLaby.laby;
-
-public class Monstre extends Personnage {
+public class Monstre extends Personnage implements Intelligence{
 
     /**
      * constructeur
@@ -9,9 +8,21 @@ public class Monstre extends Personnage {
      * @param dy position selon y
      */
     private String intelligence;
+    public Monstre(int dx, int dy,int vie) {
+        super(dx,dy,vie);
+        intelligence=Intelligence.intelligence[(int)(Math.round(Math.random()*(Intelligence.intelligence.length-1)))];
+    }
+    public Monstre(int dx, int dy,int vie,String intelligence) {
+        super(dx,dy,vie);
+        this.intelligence=intelligence;
+    }
 
-    public Monstre(int dx, int dy, int vie) {
-        super(dx, dy, vie);
-        intelligence = Intelligence.intelligence[(int) (Math.round(Math.random() * (Intelligence.intelligence.length - 1)))];
+
+    public String getIntelligence(){
+        return intelligence;
+    }
+    public void setIntelligence(String action){
+        this.intelligence=action;
     }
 }
+
